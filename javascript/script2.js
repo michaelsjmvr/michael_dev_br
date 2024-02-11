e2906ef64ae173fb692a5ef96fc5d7cfbcfde585
Michael Douglas <michaelsjmvr@hotmail.com>
2024-02-11 00:26:40 -0300

    .

--- javascript/script2.js
+++ javascript/script2.js
@@ -5,3 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
         fillBar.style.width = percentage + '%';
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var targetElement1 = document.getElementById('targetElement1');
+    var targetElement2 = document.getElementById('targetElement2');
+    var targetElement3 = document.getElementById('targetElement3');
+    var targetElement4 = document.getElementById('targetElement4');
+    
+    // Espera 2 segundos e, em seguida, muda o background-color
+    setTimeout(function() {
+      targetElement1.style.backgroundColor = 'rgba(9, 27, 42, 0.795)';
+      targetElement2.style.backgroundColor = 'rgba(9, 27, 42, 0.795)';
+      targetElement3.style.backgroundColor = 'rgba(9, 27, 42, 0.795)';
+      targetElement4.style.backgroundColor = 'rgba(9, 27, 42, 0.795)';
+    }, 500);
+  });
+  
