b5f600adaf214a15a429929d09f148f5d27c86dd
Michael Douglas <michaelsjmvr@hotmail.com>
2024-02-11 21:27:20 -0300

    .

--- javascript/script.js
+++ javascript/script.js
@@ -2,18 +2,18 @@
 window.addEventListener('resize', function () {
     if (window.innerWidth <= 576) {
         document.querySelector('.menu-header').style.display = 'none';
-        document.querySelector('.menu-footer').style.display = 'block';
+        document.querySelector('footer').style.display = 'block';
     } else {
         document.querySelector('.menu-header').style.display = 'block';
-        document.querySelector('.menu-footer').style.display = 'none';
+        document.querySelector('footer').style.display = 'none';
     }
 });
 
 // Executar uma vez na inicialização para definir o menu apropriado
 if (window.innerWidth <= 576) {
     document.querySelector('.menu-header').style.display = 'none';
-    document.querySelector('.menu-footer').style.display = 'block';
+    document.querySelector('footer').style.display = 'block';
 } else {
     document.querySelector('.menu-header').style.display = 'block';
-    document.querySelector('.menu-footer').style.display = 'none';
+    document.querySelector('footer').style.display = 'none';
 }
