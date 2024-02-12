7716e086c3fb4f3d1521dfb25a578ad5f3a4e5e5
Michael Douglas <michaelsjmvr@hotmail.com>
2024-02-12 00:32:23 -0300

    .

--- javascript/script2.js
+++ javascript/script2.js
@@ -1,3 +1,23 @@
+// JavaScript para alternar entre o menu header e o menu footer com base no tamanho da tela
+window.addEventListener('resize', function () {
+    if (window.innerWidth <= 576) {
+        document.querySelector('caixa-principal').style.display = 'none';
+        document.querySelector('footer').style.display = 'block';
+    } else {
+        document.querySelector('caixa-principal').style.display = 'block';
+        document.querySelector('footer').style.display = 'none';
+    }
+});
+
+// Executar uma vez na inicialização para definir o menu apropriado
+if (window.innerWidth <= 576) {
+    document.querySelector('caixa-principal').style.display = 'none';
+    document.querySelector('footer').style.display = 'block';
+} else {
+    document.querySelector('caixa-principal').style.display = 'block';
+    document.querySelector('footer').style.display = 'none';
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var fillBars = document.querySelectorAll('.fill.my-class3');
     fillBars.forEach(function(fillBar) {
