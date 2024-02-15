e9a551ac413fd7359dfdf4d4d3b6832b234efddb
Michael Douglas <michaelsjmvr@hotmail.com>
2024-02-14 21:14:01 -0300

    .

---
+++ javascript/script3.js
@@ -0,0 +1,23 @@
+document.getElementById("openPopup").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "block";
+});
+
+document.getElementById("closePopup").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "none";
+});
+
+document.getElementById("openPopup2").addEventListener("click", function() {
+    document.getElementById("popup2").style.display = "block";
+});
+
+document.getElementById("closePopup2").addEventListener("click", function() {
+    document.getElementById("popup2").style.display = "none";
+});
+
+document.getElementById("openPopup3").addEventListener("click", function() {
+    document.getElementById("popup3").style.display = "block";
+});
+
+document.getElementById("closePopup3").addEventListener("click", function() {
+    document.getElementById("popup3").style.display = "none";
+});
